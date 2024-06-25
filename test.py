66844dcfa528256359f32850cbddfd8f9f6f1d86
Fizur-Rahman-Fahim <137180131+Fizur-Rahman-Fahim@users.noreply.github.com>
2024-06-25 13:11:18 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-def func( **kwargs):
-    for key, value in kwargs.items():
-        print(f'{key} : {value}')   #Output: fahim : 7, kafi : 4, jashim : 15, khalid : 1
-
-result = func(fahim = 7, kafi= 4, jashim = 15, khalid = 1)
+print("Hello")
